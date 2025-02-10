03b925fa86c0b19d9030b9f45c349d04c94ef48e
shripal <shripalp@gmail.com>
2025-02-10 16:19:59 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+     <div>
+      <p className="text-2xl text-center text-red-500">
+        Hello world
+      </p>
+     
+     </div>
+    </>
+  )
+}
+
+export default App
